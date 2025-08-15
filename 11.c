ae5a83e2c83345877832db5b06644cb1943ed246
Bhavy Hirpara <bhavym1402@gmail.com>
2025-08-15 14:38:22 +0530

    Add files via upload

--- 11.c
+++ 11.c
@@ -1,40 +1,54 @@
 #include<stdio.h>
-#include<conio.h>
 
 void main()
-
 {
-    //float a,b,add,sub,mul,di;
-    
-    int num;
-    
 
-    printf("\n Enter any number between 1 to 4 :");
-    scanf("%d",&num);
+    float a,b,sum,diff,multi,divi;
+    int select;
+
+    printf("enter value of a : ");
+    scanf("%f",&a);
+
+    printf("enter value of b : ");
+    scanf("%f",&b);
+
+
+    printf("\npress 1 to get sum");
+    printf("\npress 2 to get difference");
+    printf("\npress 3 to get multi");
+    printf("\npress 4 to get division");
+
     
-        if(num ==1)
+    sum=a+b;
+    diff=a-b;
+    multi=a*b;
+    divi=a/b;
+
+    printf("\nenter value of select : ");
+    scanf("%d",&select);
+
+
+    switch(select)
+
+    {
+       
+        case 1 : printf("\nsum of a & b is : %f ",sum);
+        break;
+
         
-        {
-            printf("\n Addition ");
-        }
-        
-        else if(num ==2)
-        
-        {
-            printf("\n Substraction ");
-        }
-        
-        else if(num ==3)
-        
-        {
-            printf("\n Multiplication ");
-        }
-        
-        else 
-        
-        {
-            printf("\n Division");
-        }
-        getch();
-        
+        case 2 : printf("\ndiff of a & b is : %f ",diff);
+        break;
+
+        case 3 : printf("\nmulti of a & b is : %f ",multi);
+        break;
+
+        case 4 : printf("\ndivi of a & b is : %f ",divi);
+        break;
+
+        default : printf("\nnot posssible");
+        break;
+    }
+
+
+    getchar();
 }
