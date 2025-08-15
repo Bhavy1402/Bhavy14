b8101ad7fc4a329c4a54a22909f753307da8e94f
Bhavy Hirpara <bhavym1402@gmail.com>
2025-08-15 14:43:23 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,29 @@
+
+#include<stdio.h>
+void main()
+{
+    int num;
+    int reverse=0;
+    
+    printf("Enter Any Number : ");
+    scanf("%d",&num);
+    
+    while(num>0)
+    {
+        int rem = num % 10;//rem
+
+        reverse = reverse * 10 + rem;
+
+        num=num/10;
+        
+    }
+    
+    printf("reverse  : %d",reverse);
+    
+    
+    
+    
+    
+    getchar();
+}
+
